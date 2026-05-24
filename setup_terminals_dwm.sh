26e1e5fa94f7439dee07c220dd6629aac342efcb
erandalex <talexanderd99@gmail.com>
2026-05-24 13:26:26 -0500

    Split work timer into 15m to start + the rest of time, and added encouraging message text + audio to kindly motivate myself to maintain the work habit for at least 15m a day

--- setup_terminals_dwm.sh
+++ setup_terminals_dwm.sh
@@ -2,11 +2,15 @@
 
 WORK_TIME_HRS=1
 WORK_TIME_MIN=35
+START_OF_WORK_TIME_BLOCK_MIN=15
+
+ENCOURAGING_MESSAGE="***Please work for ${START_OF_WORK_TIME_BLOCK_MIN} minutes, even if you are tired or feel pressed for time. This maintains the habit & makes consistent progress. You deserve this happiness.***"
+say "$ENCOURAGING_MESSAGE" &
 
 SESSION="daily"
 
 tmux new-session -d -s "$SESSION" -x "$(tput cols)" -y "$(tput lines)" "/home/erandalex/main/todo/.infra/begin_daily_workflow"
-tmux split-window -t "$SESSION" -v -p 10 "countdown -hr $WORK_TIME_HRS -m $WORK_TIME_MIN"
+tmux split-window -t "$SESSION" -v -p 10 "echo '$ENCOURAGING_MESSAGE'; countdown -m $START_OF_WORK_TIME_BLOCK_MIN; countdown -hr $WORK_TIME_HRS -m $((WORK_TIME_MIN - START_OF_WORK_TIME_BLOCK_MIN))"
 tmux select-pane -t "$SESSION:0.0"
 
 $TERMINAL_COMMAND -e tmux attach-session -t "$SESSION"
